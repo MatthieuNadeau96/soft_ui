59edaf839da2d49c74fbae5c66c20750ed9f56b9
Matthieu <matthieunadeau96@gmail.com>
2020-04-23 12:58:52 -0400

    Added shadows to give it neumorphism

--- lib/main.dart
+++ lib/main.dart
@@ -26,10 +26,24 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Container(
           width: 200,
           height: 200,
-          child: Icon(
-            Icons.android,
-            size: 80,
-          ),
+          child: Icon(Icons.android, size: 80),
+          decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.all(Radius.circular(50)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey[500],
+                  offset: Offset(4.0, 4.0),
+                  blurRadius: 15.0,
+                  spreadRadius: 1.0,
+                ),
+                BoxShadow(
+                  color: Colors.white,
+                  offset: Offset(-4.0, -4.0),
+                  blurRadius: 15.0,
+                  spreadRadius: 1.0,
+                )
+              ]),
         ),
       ),
     );
