5bb5a9c00123803a5333cacc59992aebac0c5bb2
Matthieu <matthieunadeau96@gmail.com>
2020-04-23 13:14:43 -0400

    added gesture detector to the button

--- lib/main.dart
+++ lib/main.dart
@@ -18,32 +18,43 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool isPressed = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: Center(
-        child: Container(
-          width: 200,
-          height: 200,
-          child: Icon(Icons.android, size: 80),
-          decoration: BoxDecoration(
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              isPressed = !isPressed;
+            });
+          },
+          child: Container(
+            width: 200,
+            height: 200,
+            child: Icon(Icons.android, size: 80),
+            decoration: BoxDecoration(
               color: Colors.grey[300],
               borderRadius: BorderRadius.all(Radius.circular(50)),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey[500],
-                  offset: Offset(4.0, 4.0),
-                  blurRadius: 15.0,
-                  spreadRadius: 1.0,
-                ),
-                BoxShadow(
-                  color: Colors.white,
-                  offset: Offset(-4.0, -4.0),
-                  blurRadius: 15.0,
-                  spreadRadius: 1.0,
-                )
-              ]),
+              boxShadow: isPressed == true
+                  ? null
+                  : [
+                      BoxShadow(
+                        color: Colors.grey[500],
+                        offset: Offset(4.0, 4.0),
+                        blurRadius: 15.0,
+                        spreadRadius: 1.0,
+                      ),
+                      BoxShadow(
+                        color: Colors.white,
+                        offset: Offset(-4.0, -4.0),
+                        blurRadius: 15.0,
+                        spreadRadius: 1.0,
+                      )
+                    ],
+            ),
+          ),
         ),
       ),
     );
